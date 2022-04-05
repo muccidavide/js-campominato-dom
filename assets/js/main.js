8643e5c9d72e8ecb6b07f2a22d5f8b10ad4960de
muccidavide <davidemucci0804@gmail.com>
2022-04-05 12:01:47 +0200

    Inital Commit: improved reset function and cell n°

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+// Funzione creazione griglia
+let cellsElement = document.querySelector('.cells');
+
+let submitClick = document.getElementById('play')
+
+let userDifficultyChoice = submitClick.addEventListener('click', function(){
+    let difficultySelectorElement = document.getElementById('difficulty').value;
+    let cellNumber;
+
+    //Reset Griglia e valori
+    cellsElement.innerHTML = '';
+    
+    //Livello di difficolta
+    if (difficultySelectorElement === 'easy') {
+        cellNumber = 100;
+        cellForRowNumber = 10;
+    } else if (difficultySelectorElement === 'medium'){
+        cellNumber = 81;
+        cellForRowNumber = 9;
+    } else {
+        cellNumber = 49;
+        cellForRowNumber = 7;
+    }
+
+    generateGrid(cellsElement, 'div', 'cell', cellNumber)
+    activateCell('.cell')
+    
+     
+})
+
+
+function generateGrid(selector, tag_name, class_name, limit) {
+
+    const gameAreaElement = document.querySelector('.cells')
+
+    // pulire area di gioco
+    gameAreaElement.innerHTML = ''
+  
+    for (let index = 1; index <= limit; index++) {
+        let cellItem = document.createElement(tag_name);
+        selector.append(cellItem);
+        cellItem.classList.add(class_name);
+        cellItem.style.width = `calc(100% / ${cellForRowNumber})`
+        cellItem.append(index)
+
+        gameAreaElement.append(cellItem)   
+    } 
+}
+
+function activateCell(class_name) {
+    const cells = document.querySelectorAll(class_name)
+    //console.log(cells);
+  
+    for (let index = 0; index < cells.length; index++) {
+      const cell = cells[index];
+      cell.addEventListener('click', function () {
+        cell.style.backgroundColor = 'cornflowerblue';
+        cell.style.color = 'white';
+
+      })
+    }
+}
+
